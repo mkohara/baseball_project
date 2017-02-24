828d3cf266b5d95a6cfc71196741ff58ecb6b198
Raghu Betina <raghu@firstdraft.com>
2017-02-24 16:09:38 +0000

    Whitelist columns in Player for ActiveAdmin

--- app/admin/player.rb
+++ app/admin/player.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Player do
 
+ permit_params :statistics, :scout_id, :team_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
