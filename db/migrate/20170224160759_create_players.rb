59923b6991402e3ef9de670bc56cb68bc824ef67
Raghu Betina <raghu@firstdraft.com>
2017-02-24 16:07:59 +0000

    Generate players resource with starter:resource

---
+++ db/migrate/20170224160759_create_players.rb
@@ -0,0 +1,12 @@
+class CreatePlayers < ActiveRecord::Migration
+  def change
+    create_table :players do |t|
+      t.string :statistics
+      t.integer :scout_id
+      t.integer :team_id
+
+      t.timestamps
+
+    end
+  end
+end
